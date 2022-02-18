3d85a0e9e6572ec16bd85a8165697bbac277de6d
mohammed-sada <mohammedabusada94@example.com>
2022-02-18 22:02:25 +0200

    heart video featuer added

--- pages/video/[videoId].tsx
+++ pages/video/[videoId].tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 import Modal from 'react-modal';
+import { AiFillHeart, AiOutlineHeart } from 'react-icons/ai';
 
 import { getVideoById } from '../../lib/videos';
 import { Videos } from '../../types';
@@ -14,7 +15,7 @@ export async function getStaticProps({ params }: any) {
     props: {
       video: video.length > 0 ? video[0] : {},
     },
-    revalidate: 10, // In seconds
+    revalidate: 10, // In seconds ISR
   };
 }
 
@@ -33,12 +34,55 @@ export default function Video({ video }: { video: typeof Videos[0] }) {
   const router = useRouter();
   const { videoId } = router.query;
 
+  const [toggleHeart, setToggleHeart] = useState(false);
+  const [loadingHeart, setLoadingHeart] = useState(false);
+
+  useEffect(() => {
+    async function getCurrentVideoStats() {
+      try {
+        const res = await fetch(`/api/stats?videoId=${videoId}`, {
+          method: 'GET',
+        });
+        const { data } = await res.json();
+        setToggleHeart(data.favourited);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getCurrentVideoStats();
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const heartVideo = async () => {
+    try {
+      setLoadingHeart(true);
+      const res = await fetch('/api/stats', {
+        method: 'POST',
+        body: JSON.stringify({
+          videoId,
+          favourited: !toggleHeart,
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      const data = await res.json();
+
+      setToggleHeart((prev) => !prev);
+      setLoadingHeart(false);
+    } catch (error) {
+      console.log(error);
+      setLoadingHeart(false);
+    }
+  };
   return (
     <>
       <Head>
-        <title>video title</title>
-        <meta name='description' content='video desc' />
+        <title>{video.channelTitle}</title>
+        <meta name={video.channelTitle} content={video.description} />
       </Head>
+
       <Navbar />
       <Modal
         isOpen={true}
@@ -47,14 +91,27 @@ export default function Video({ video }: { video: typeof Videos[0] }) {
         overlayClassName='absolute top-24 w-screen min-h-screen flex justify-center items-center'
         className='w-3/4 min-h-screen shadow-2xl'
       >
-        <iframe
-          id='ytplayer'
-          width='100%'
-          height='360'
-          src={`https://www.youtube.com/embed/${videoId}?autoplay=1&controls=0&modestbranding=1&rel=0&theme=light&color=white&origin=http://example.com`}
-          frameBorder='0'
-          allowFullScreen
-        />
+        <div className='relative group'>
+          <iframe
+            id='ytplayer'
+            width='100%'
+            height='360'
+            src={`https://www.youtube.com/embed/${videoId}?autoplay=1&controls=0&modestbranding=1&rel=0&theme=light&color=white&origin=http://example.com`}
+            frameBorder='0'
+            allowFullScreen
+          />
+          <button
+            className='absolute top-[40%] left-6 text-bg outline-none scale-0 hover:scale-110 transition-all disabled:opacity-50 group-hover:scale-100'
+            disabled={loadingHeart}
+            onClick={heartVideo}
+          >
+            {toggleHeart ? (
+              <AiFillHeart size={80} />
+            ) : (
+              <AiOutlineHeart size={80} />
+            )}
+          </button>
+        </div>
 
         <div className='p-10 mt-4 flex'>
           <div className='pr-4 w-2/3 text-xl font-semibold'>
